a7ab59d9049afffe13d9a47a48ef0db6d7aa5866
pap0gallo <khodunov14@gmail.com>
2025-06-23 17:59:22 +0300

    add exercise from lesson 15

---
+++ src/Time.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+class Time
+{
+    private $h;
+    private $m;
+
+    // BEGIN (write your solution here)
+    public static function __fromString($time)
+    {
+        [$h, $m] = explode(':', $time);
+        return new self($h, $m);
+    }
+    // END
+
+    public function __construct($h, $m)
+    {
+        $this->h = $h;
+        $this->m = $m;
+    }
+
+    public function __toString()
+    {
+        return "{$this->h}:{$this->m}";
+    }
+}
